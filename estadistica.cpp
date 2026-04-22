f112ba1340e0bd9f4b9309e5c14d27c863cb80d4
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-04-22 00:14:32 -0500

    Update estadistica.cpp
    Implementacion de la estructura definida en estadistica.h

--- estadistica.cpp
+++ estadistica.cpp
@@ -1,3 +1,123 @@
-#include "estadistica.h"
+#include "Estadistica.h"
 
-Estadistica::Estadistica() {}
+// Constructor por defecto
+Estadistica::Estadistica()
+    : golesFavor(0),
+    golesContra(0),
+    partidosGanados(0),
+    partidosEmpatados(0),
+    partidosPerdidos(0),
+    tarjetasAmarillas(0),
+    tarjetasRojas(0),
+    faltas(0),
+    minutosJugados(0),
+    asistencias(0)
+{
+}
+
+// Constructor con parametros
+Estadistica::Estadistica(unsigned short int gFavor,
+                         unsigned short int gContra,
+                         unsigned short int ganados,
+                         unsigned short int empatados,
+                         unsigned short int perdidos,
+                         unsigned short int amarillas,
+                         unsigned short int rojas,
+                         unsigned short int faltasPartido,
+                         unsigned short int minutos,
+                         unsigned short int asistenciasPartido)
+    : golesFavor(gFavor),
+    golesContra(gContra),
+    partidosGanados(ganados),
+    partidosEmpatados(empatados),
+    partidosPerdidos(perdidos),
+    tarjetasAmarillas(amarillas),
+    tarjetasRojas(rojas),
+    faltas(faltasPartido),
+    minutosJugados(minutos),
+    asistencias(asistenciasPartido)
+{
+}
+
+// Constructor de copia
+Estadistica::Estadistica(const Estadistica& otras)
+    : golesFavor(otras.golesFavor),
+    golesContra(otras.golesContra),
+    partidosGanados(otras.partidosGanados),
+    partidosEmpatados(otras.partidosEmpatados),
+    partidosPerdidos(otras.partidosPerdidos),
+    tarjetasAmarillas(otras.tarjetasAmarillas),
+    tarjetasRojas(otras.tarjetasRojas),
+    faltas(otras.faltas),
+    minutosJugados(otras.minutosJugados),
+    asistencias(otras.asistencias)
+{
+}
+
+
+Estadistica::~Estadistica()
+{
+    // No hay memoria dinamica
+}
+
+
+unsigned short int Estadistica::getGolesFavor() const { return golesFavor; }
+unsigned short int Estadistica::getGolesContra() const { return golesContra; }
+unsigned short int Estadistica::getPartidosGanados() const { return partidosGanados; }
+unsigned short int Estadistica::getPartidosEmpatados() const { return partidosEmpatados; }
+unsigned short int Estadistica::getPartidosPerdidos() const { return partidosPerdidos; }
+unsigned short int Estadistica::getTarjetasAmarillas() const { return tarjetasAmarillas; }
+unsigned short int Estadistica::getTarjetasRojas() const { return tarjetasRojas; }
+unsigned short int Estadistica::getFaltas() const { return faltas; }
+unsigned short int Estadistica::getMinutosJugados() const { return minutosJugados; }
+unsigned short int Estadistica::getAsistencias() const { return asistencias; }
+
+
+void Estadistica::actualizar(const Estadistica& otras)
+{
+    golesFavor += otras.golesFavor;
+    golesContra += otras.golesContra;
+    partidosGanados += otras.partidosGanados;
+    partidosEmpatados += otras.partidosEmpatados;
+    partidosPerdidos += otras.partidosPerdidos;
+    tarjetasAmarillas += otras.tarjetasAmarillas;
+    tarjetasRojas += otras.tarjetasRojas;
+    faltas += otras.faltas;
+    minutosJugados += otras.minutosJugados;
+    asistencias += otras.asistencias;
+}
+
+int Estadistica::diferenciaGoles() const
+{
+    // Conversion a int
+    return static_cast<int>(golesFavor) - static_cast<int>(golesContra);
+}
+
+Estadistica& Estadistica::operator+=(const Estadistica& otras)
+{
+    actualizar(otras);
+    return *this;
+}
+
+Estadistica Estadistica::operator+(const Estadistica& otras) const
+{
+    Estadistica resultado(*this);  // Copia el objeto actual
+    resultado += otras;
+    return resultado;
+}
+
+
+ostream& operator<<(ostream& os, const Estadistica& stats)
+{
+    os << "Goles F: " << stats.golesFavor
+       << " | Goles C: " << stats.golesContra
+       << " | PG: " << stats.partidosGanados
+       << " | PE: " << stats.partidosEmpatados
+       << " | PP: " << stats.partidosPerdidos
+       << " | TA: " << stats.tarjetasAmarillas
+       << " | TR: " << stats.tarjetasRojas
+       << " | Faltas: " << stats.faltas
+       << " | Min: " << stats.minutosJugados
+       << " | Asist: " << stats.asistencias;
+    return os;
+}
