dc1cc13773acbd24dce6ca79994e173e1bf36a13
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-04-22 21:17:22 -0500

    Update lista.h
    Definicion de los atributos y respectivos metodos de la clase lista.

--- lista.h
+++ lista.h
@@ -1,10 +1,41 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+template <typename T>
 class Lista
 {
+private:
+    T** elementos;     // Arreglo dinamico de punteros a T
+    int capacidad;     // Tamaño max
+    int tamanio;       // Cantidad de elementos
+
 public:
+    // Constructor
     Lista();
+
+    // Constructor de copia
+    Lista(const Lista<T>& otra);
+
+    // Destructor
+    ~Lista();
+
+    // Consultas básicas
+    int obtenerCapacidad() const;
+    int obtenerTamanio() const;
+    bool estaVacia() const;
+
+    void agregar(T* elemento);
+    T* obtener(int indice) const;
+    T* operator[](int indice) const;  
+
+    void eliminar(int indice);
+    void limpiar(); 
+
+    // Operador de asignacion
+    Lista<T>& operator=(const Lista<T>& otra);
+
 };
 
-#endif // LISTA_H
+#include "lista.cpp"
+
+#endif
