ed75e7faf2bf39657017b3752b0d063b8f5462dd
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-04-25 11:56:11 -0500

    Update equipo.h

--- equipo.h
+++ equipo.h
@@ -1,10 +1,40 @@
 #ifndef EQUIPO_H
 #define EQUIPO_H
 
-class Equipo
-{
+#include <string>
+#include "Estadistica.h"
+#include "Lista.h"
+#include "Jugador.h"
+
+class Equipo {
+private:
+    std::string nombrePais;
+    std::string confederacion;
+    unsigned short int rankingFIFA;
+    std::string directorTecnico;  
+    Estadistica estadisticasHist;
+    Lista<Jugador*> plantilla;
+    
+    Equipo(const Equipo&);
+    Equipo& operator=(const Equipo&);
+    
 public:
     Equipo();
+    Equipo(const std::string& nombre, const std::string& conf, 
+           unsigned short int ranking, const std::string& dt);
+    ~Equipo(); 
+    
+    void actualizarEstadisticas(const Estadistica& statsPartido);
+    
+    Lista<Jugador*> obtenerGoleadores() const;
+    
+    float obtenerPromedioGolesFavor() const;
+    float obtenerPromedioGolesContra() const;
+    
+    std::string getNombre() const;
+    unsigned short int getRanking() const;
+    
+    void agregarJugador(Jugador* j);
 };
 
-#endif // EQUIPO_H
+#endif
