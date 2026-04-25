fd3e6f87354ce9002a6195643bccc8aede0a3cad
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-04-24 23:13:19 -0500

    Update jugador.h
    Definicion de los atributos y metodos de la clase jugador

--- jugador.h
+++ jugador.h
@@ -1,10 +1,29 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
+#include "estadistica.h"
+#include <string>
 
 class Jugador
 {
+private:
+    std::string nombre;
+    std::string apellido;
+    unsigned short int numeroCamiseta;
+    Estadistica estadisticas;
+
 public:
-    Jugador();
+    //Constructor
+    Jugador(const std::string& nombre, const std::string& apellido, unsigned short int numeroCamiseta);
+
+    // Getters
+    std::string getNombre() const;
+    std::string getApellido() const;
+    unsigned short int getNumeroCamiseta() const;
+    const Estadistica& getEstadisticas() const;
+    unsigned short int getGoles() const;
+
+    // estadisticas que hizo en el partido
+    void actualizarEstadisticas(const Estadistica& statsPartido);
 };
 
 #endif // JUGADOR_H
