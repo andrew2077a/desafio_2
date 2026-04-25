b6a0353371eadac87ff7e31dd8d3ae21f2de8212
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-04-24 23:14:23 -0500

    Update jugador.cpp
    Implementacion de los constructores, setters, getters y funciones utiles de la clase jugador

--- jugador.cpp
+++ jugador.cpp
@@ -1,3 +1,34 @@
 #include "jugador.h"
+using namespace std;
 
-Jugador::Jugador() {}
+// Constructor
+Jugador::Jugador(const string& nombre, const string& apellido, unsigned short int numeroCamiseta)
+    : nombre(nombre), apellido(apellido), numeroCamiseta(numeroCamiseta), estadisticas() {
+    // Todo se inicializa en 0 gracias al constructor de Estadistica
+}
+
+// Getters
+string Jugador::getNombre() const{
+    return nombre;
+}
+
+string Jugador::getApellido() const{
+    return apellido;
+}
+
+unsigned short int Jugador::getNumeroCamiseta() const{
+    return numeroCamiseta;
+}
+
+const Estadistica& Jugador::getEstadisticas() const{
+    return estadisticas;
+}
+
+unsigned short int Jugador::getGoles() const {
+    return estadisticas.getGolesFavor();
+}
+
+// Actualizar estadisticas (acumula las del partido)
+void Jugador::actualizarEstadisticas(const Estadistica& statsPartido) {
+    estadisticas.actualizar(statsPartido);
+}
